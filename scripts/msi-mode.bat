2fcb8dd6a4e60490fd67b7a5e605f3cc4d84d28a
kelvenapk <kelvenpinheiro710@gmail.com>
2025-11-10 20:30:02 -0300

    Update msi-mode.bat

--- scripts/msi-mode.bat
+++ scripts/msi-mode.bat
@@ -1,1 +1,11 @@
+@echo off
+title MSI Mode - Kelven Optimizer
+:: Ativa MSI Mode para GPU - reduz DPC latency
 
+:: NVIDIA
+reg add "HKLM\SYSTEM\CurrentControlSet\Enum\PCI\VEN_10DE&DEV_*\Device Parameters\Interrupt Management\MessageSignaledInterruptProperties" /v "MSISupported" /d 1 /t REG_DWORD /f >nul 2>&1
+
+:: AMD
+reg add "HKLM\SYSTEM\CurrentControlSet\Enum\PCI\VEN_1002&DEV_*\Device Parameters\Interrupt Management\MessageSignaledInterruptProperties" /v "MSISupported" /d 1 /t REG_DWORD /f >nul 2>&1
+
+exit
