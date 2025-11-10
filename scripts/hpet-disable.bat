b7b0715ec6ad96e24fad3f2da0417cc7f7ce6f69
kelvenapk <kelvenpinheiro710@gmail.com>
2025-11-10 20:30:41 -0300

    Update hpet-disable.bat

--- scripts/hpet-disable.bat
+++ scripts/hpet-disable.bat
@@ -1,1 +1,9 @@
+@echo off
+title HPET Disable - Kelven Optimizer
+:: Desativa HPET - reduz latência
 
+bcdedit /set useplatformclock false >nul 2>&1
+bcdedit /set disabledynamictick yes >nul 2>&1
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverride" /d 3 /t REG_DWORD /f >nul 2>&1
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverrideMask" /d 3 /t REG_DWORD /f >nul 2>&1
+exit
