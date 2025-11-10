9a248b3e488a67cd3f153e84819a7be8b27f6c2e
kelvenapk <kelvenpinheiro710@gmail.com>
2025-11-10 20:29:10 -0300

    Update servicos-do-windows.ps1

--- scripts/servicos-do-windows.ps1
+++ scripts/servicos-do-windows.ps1
@@ -1,1 +1,22 @@
+# Serviços do Windows - Kelven Optimizer
+# Desativa apenas serviços DESNECESSÁRIOS e NÃO CRÍTICOS
 
+$servicesToDisable = @(
+    "Fax",
+    "WerSvc",           # Windows Error Reporting
+    "DiagTrack",        # Telemetria
+    "dmwappushservice", # WAP Push Message Routing Service
+    "MapsBroker",       # Downloaded Maps Manager
+    "lfsvc",            # Geolocation Service
+    "SharedAccess",     # Internet Connection Sharing
+    "TrkWks",           # Distributed Link Tracking Client
+    "WbioSrvc",         # Windows Biometric Service (se não usar)
+    "WerSvc"            # Windows Error Reporting (repetido por segurança)
+)
+
+foreach ($svc in $servicesToDisable) {
+    try {
+        Set-Service -Name $svc -StartupType Disabled -ErrorAction SilentlyContinue
+        Stop-Service -Name $svc -Force -ErrorAction SilentlyContinue
+    } catch {}
+}
