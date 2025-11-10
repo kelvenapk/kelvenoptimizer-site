f948d571a5d7f8ab5d0975bdac65ab25c2ad7366
kelvenapk <kelvenpinheiro710@gmail.com>
2025-11-10 20:29:45 -0300

    Update registro-do-sistema.ps1

--- scripts/registro-do-sistema.ps1
+++ scripts/registro-do-sistema.ps1
@@ -1,1 +1,16 @@
+# Registro do Sistema - Kelven Optimizer
+# Remove apenas entradas órfãs seguras
 
+$keysToClean = @(
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU",
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\TypedPaths",
+    "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache",
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU",
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedPidlMRU"
+)
+
+foreach ($key in $keysToClean) {
+    if (Test-Path $key) {
+        Remove-ItemProperty -Path $key -Name "*" -ErrorAction SilentlyContinue
+    }
+}
