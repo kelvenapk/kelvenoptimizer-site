c101494e8efdcb83e5b32e8b92db668b8ff7e3b7
kelvenapk <kelvenpinheiro710@gmail.com>
2025-11-10 20:28:45 -0300

    Update efeitos-visuais.bat

--- scripts/efeitos-visuais.bat
+++ scripts/efeitos-visuais.bat
@@ -1,1 +1,14 @@
+@echo off
+title Efeitos Visuais - Kelven Optimizer
+:: Desativa animações e efeitos pesados - mantém funcionalidade
 
+reg add "HKCU\Software\Microsoft\Windows\DWM" /v "EnableAeroPeek" /d 0 /t REG_DWORD /f >nul 2>&1
+reg add "HKCU\Control Panel\Desktop" /v "MenuShowDelay" /d 0 /t REG_SZ /f >nul 2>&1
+reg add "HKCU\Control Panel\Desktop" /v "UserPreferencesMask" /d 90 32 03 80 10 00 00 00 /t REG_BINARY /f >nul 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" /v "VisualFXSetting" /d 2 /t REG_DWORD /f >nul 2>&1
+reg add "HKCU\Control Panel\Desktop\WindowMetrics" /v "MinAnimate" /d 0 /t REG_SZ /f >nul 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "IconsOnly" /d 1 /t REG_DWORD /f >nul 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ListviewAlphaSelect" /d 0 /t REG_DWORD /f >nul 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ListviewShadow" /d 0 /t REG_DWORD /f >nul 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarAnimations" /d 0 /t REG_DWORD /f >nul 2>&1
+exit
