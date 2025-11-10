7b6b01ec9d574fb1449ead8f0ac8835cb42edebb
kelvenapk <kelvenpinheiro710@gmail.com>
2025-11-10 20:29:28 -0300

    Update inicializacao.ps1

--- scripts/inicializacao.ps1
+++ scripts/inicializacao.ps1
@@ -1,1 +1,16 @@
+# Inicialização - Kelven Optimizer
+# Remove apenas programas NÃO ESSENCIAIS do startup
 
+$startupPaths = @(
+    "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup",
+    "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+)
+
+$whitelist = @("Windows Security", "OneDrive", "Spotify", "Discord", "Steam", "Epic Games Launcher")
+
+Get-ChildItem -Path $startupPaths -Filter *.lnk -ErrorAction SilentlyContinue | ForEach-Object {
+    $name = $_.BaseName
+    if ($whitelist -notcontains $name) {
+        Remove-Item $_.FullName -Force -ErrorAction SilentlyContinue
+    }
+}
