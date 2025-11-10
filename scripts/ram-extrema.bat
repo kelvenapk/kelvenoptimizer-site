cdd012260b9bca631c72a791d7129de2f8712c8a
kelvenapk <kelvenpinheiro710@gmail.com>
2025-11-10 20:31:15 -0300

    Update ram-extrema.bat

--- scripts/ram-extrema.bat
+++ scripts/ram-extrema.bat
@@ -1,1 +1,21 @@
+@echo off
+title RAM Extrema - Kelven Optimizer
+:: Otimiza memória + desativa coisas pesadas
 
+sc config SysMain start=disabled >nul 2>&1
+net stop SysMain >nul 2>&1
+sc config "WerSvc" start=disabled >nul 2>&1
+net stop WerSvc >nul 2>&1
+sc config "DiagTrack" start=disabled >nul 2>&1
+net stop DiagTrack >nul 2>&1
+sc config "MapsBroker" start=disabled >nul 2>&1
+net stop MapsBroker >nul 2>&1
+sc config "lfsvc" start=disabled >nul 2>&1
+net stop lfsvc >nul 2>&1
+sc config "SharedAccess" start=disabled >nul 2>&1
+net stop SharedAccess >nul 2>&1
+sc config "TrkWks" start=disabled >nul 2>&1
+net stop TrkWks >nul 2>&1
+sc config "WbioSrvc" start=disabled >nul 2>&1
+net stop WbioSrvc >nul 2>&1
+exit
