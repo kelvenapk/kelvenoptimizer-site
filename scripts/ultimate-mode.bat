1becde52b9f8cffef32038a560fa64451b6f6e7c
kelvenapk <kelvenpinheiro710@gmail.com>
2025-11-10 20:31:30 -0300

    Update ultimate-mode.bat

--- scripts/ultimate-mode.bat
+++ scripts/ultimate-mode.bat
@@ -1,1 +1,19 @@
+@echo off
+title ULTIMATE MODE - Kelven Optimizer
+:: Executa TODAS as otimizações anteriores em sequência
 
+start /b "" "%~dp0cpu-extrema.bat"
+start /b "" "%~dp0ram-extrema.bat"
+start /b "" "%~dp0msi-mode.bat"
+start /b "" "%~dp0timer-resolution.bat"
+start /b "" "%~dp0hpet-disable.bat"
+start /b "" "%~dp0game-mode.bat"
+start /b "" "%~dp0limpeza-de-sistema.bat"
+start /b "" "%~dp0otimizacao-de-rede.bat"
+start /b "" "%~dp0servicos-do-windows.ps1"
+start /b "" "%~dp0inicializacao.ps1"
+start /b "" "%~dp0registro-do-sistema.ps1"
+start /b "" "%~dp0efeitos-visuais.bat"
+
+echo ✅ ULTIMATE MODE aplicado!
+exit
